d1366366a518ca84203c528a0eac8d5a9026e84e
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2023-09-04 23:03:41 -0400

    responsiveness

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Card.css'; // You should create a CSS file for styling
+import './Card.css'; // Your CSS file for styling
 
 const Card = () => {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -12,10 +12,16 @@ const Card = () => {
     <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleCardClick}>
       <div className="card-inner">
         <div className="card-front">
-          <img src={require("./Images/card-front.png")} alt="Card Front" />
+          <img
+            src={window.innerWidth > 768 ? require('./Images/card-front.png') : require('./Images/card-front-vert.png')}
+            alt="Card Front"
+          />
         </div>
         <div className="card-back">
-          <img src={require("./Images/card-back.png")} alt="Card Back" />
+          <img
+            src={window.innerWidth > 768 ? require('./Images/card-back.png') : require('./Images/card-back-vert.png')}
+            alt="Card Back"
+          />
         </div>
       </div>
     </div>
